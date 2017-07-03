78972f195a7770f8a7909749f7171d0673a1b4be
sg255076 <sachin.gade@teradata.com>
2017-07-03 15:46:44 +0530

    Added html changes

--- flask-hellow-world/app.py
+++ flask-hellow-world/app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return '<html><h1>Hello Sachin Gade! I have been seen {} times.\n</h1></html>'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
